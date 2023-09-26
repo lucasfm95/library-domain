55c6bee6c37ad19d0e6c9105a837702c7823335c
Lucas Martins <luucasmartins@hotmail.com>
2023-09-26 20:40:31 -0300

    added first unit test

---
+++ LibraryDomain.Tests/AccountDataTest.cs
@@ -0,0 +1,30 @@
+using Bogus;
+using FluentAssertions;
+using LibraryDomain.Data;
+
+namespace LibraryDomain.Tests;
+
+public class AccountDataTest
+{
+    [Fact]
+    public void GenerateAccountData()
+    {
+
+        var transactionAccountData = new Faker<TransactionAccountData>("pt_BR")
+            .RuleFor(property => property.Id, func => func.Random.Int(1,100000));
+
+
+        var customerData = new Faker<CustomerData>("pt_BR")
+            .RuleFor(x => x.FirstName, f => f.Name.FullName());
+
+        
+        var accountsData = new Faker<AccountData>("pt_BR")
+            .RuleFor(property => property.Balance, f => f.Random.Int(1,100000))
+            .RuleFor(property => property.BankName, f => f.Name.FullName())
+            .RuleFor(property => property.Number, f => f.Finance.Account())
+            .RuleFor(property => property.Customer, f => customerData)
+            .RuleFor(property => property.Transactions, func => transactionAccountData.Generate(500000000))
+            .Generate(500);
+
+    }
+}
--- LibraryDomain/Data/CustomerData.cs
+++ LibraryDomain/Data/CustomerData.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-namespace LibraryDomain.Data
-{
-	public class CustomerData
-	{
-		public string? FirstName { get; set; }
-		public string? LastName { get; set; }
-		public DateTime CreatedAt { get; set; }
-		public string? DocumentNumber { get; set; }
-		public AddressData? Address { get; set; }
-	}
-}
+namespace LibraryDomain.Data;
 
+public class CustomerData
+{
+	public string? FirstName { get; set; }
+	public string? LastName { get; set; }
+	public DateTime CreatedAt { get; set; }
+	public string? DocumentNumber { get; set; }
+	public AddressData? Address { get; set; }
+}
